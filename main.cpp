985aaad5183f0be112f5620ee5b90edc3268d13c
TonyLiu <ltsplice@gmail.com>
2023-02-16 13:30:05 -0500

    updated trapezoid3x3

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 #include "lower.h"
 #include "upper.h"
 #include "trapezoid.h"
+#include "checkerboard3x3.h"
 
 int main(){
-    std::cout <<  box(5,5);
+    std::cout << box(5,5);
     std::cout << checkerboard(5,5);
     std::cout << cross(5);
     std::cout << lower(5);
     std::cout << upper(5);
     std::cout << trapezoid(10,5);
+    std::cout << checkerboard3x3(15,15);
     return 0;
 }
