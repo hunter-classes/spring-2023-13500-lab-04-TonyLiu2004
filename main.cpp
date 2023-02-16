37f8d77cdae61ee6e4b0b93f9d66a4c91f57866c
TonyLiu <ltsplice@gmail.com>
2023-02-16 13:15:56 -0500

    upper update

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include "checkerboard.h"
 #include "cross.h"
 #include "lower.h"
+#include "upper.h"
 
 int main(){
     std::cout <<  box(5,5);
     std::cout << checkerboard(5,5);
     std::cout << cross(5);
     std::cout << lower(5);
+    std::cout << upper(5);
     return 0;
 }
